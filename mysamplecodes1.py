056a593e150b8bcf818bc6a6f9f28c2c12435d77
fedegit25 <federico_ivan.brigante@uochb.cas.cz>
2025-05-27 10:55:31 +0200

    Update mysamplecodes1.py

--- mysamplecodes1.py
+++ mysamplecodes1.py
@@ -26,7 +26,7 @@ csv_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
 database_df = pd.read_csv(csv_url, on_bad_lines='skip')
 
 database_df = database_df.astype(str)
-sheet_url = st.secrets["private_gsheets_url"] #this information should be included in streamlit secret
+sheet_url = st.secrets["https://www.googleapis.com/oauth2/v1/certs"] #this information should be included in streamlit secret
 sheet = client.open_by_url(sheet_url).sheet1
 sheet.update([database_df.columns.values.tolist()] + database_df.values.tolist())
 st.success('Data has been written to Google Sheets')
